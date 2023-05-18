1c404390ffc3339db4573ae8bcddb9761505cd74
el a5tabot <youssef20191700793@cis.asu.edu.eg>
2023-05-18 02:13:09 +0200

    Read the data
    signal values in signals list
    channel of the signal in signal_names list
    signal class in signal_classes list (up,down,right,left and blink)
    
    Co-authored-by: Kerolosnabil7 <Kerolosnabil744@gmail.com>
    Co-authored-by: Omran28 <mahmoudomran36@gmail.com>

--- main.py
+++ main.py
@@ -1,81 +1,40 @@
 # Imports
 import os
 import statistics
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from scipy import signal
 from scipy.signal import butter, filtfilt
-
-# Filtering
-def butter_bandpass_filter(Input_Signal, Low_Cutoff, High_Cutoff, Sampling_Rate, order):
-    nyq = 0.5 * Sampling_Rate  # Nyquist Sampling
-    low = Low_Cutoff / nyq
-    high = High_Cutoff / nyq
-    Numerator, denominator = butter(order, [low, high], btype='band', output='ba', analog=False, fs=None)
-    # Passing the 1st Column of data shape (251,) instead of (251,1)
-    filtered = filtfilt(Numerator, denominator, Input_Signal)
-    return filtered
+import preprocessing as pp
 
 # Read Files
 path = "3-class"
-signals = []
-c = -1
-
-for file in os.listdir(path):
-    c += 1
-    EOG_signal = open(path + "\\" + file, 'r')
-    signals.append(EOG_signal.readlines())
-
-sig = []
-for i in range(len(signals)):
-    temp = []
-    for j in range(len(signals[0])-1):
-        temp.append(int(signals[i][j + 1]))
-    sig.append(temp)
 
 
-# Preprocessing
 
-# Signals Filtering
-filtered_signal = []
-for i in range(len(sig)):
-    filtered_signal.append(butter_bandpass_filter(sig[i], Low_Cutoff=0.5, High_Cutoff=20.0, Sampling_Rate=176, order=2))
+def read_sig(folder_path):
+    signals_f = []
+    signals_name_f = []
+    channel_f = []
+    list_of_classes = ["asagi", "kirp", "sag", "sol", "yukari"]  # The classes name we interested in
+    files = os.listdir(path)
 
-plt.figure(figsize=(12, 6))
-plt.plot(np.arange(0, len(filtered_signal[0])), filtered_signal[0])
-plt.xlabel("Time(s)")
-plt.ylabel("Amp (v)")
-plt.show()
-
-# Signals Resampling
-resampled_signals = []
-for i in range(len(filtered_signal)):
-    resampled_signals.append(signal.resample(filtered_signal[i], 50))
-
-plt.figure(figsize=(12, 6))
-plt.subplot(121)
-plt.plot(np.arange(0, len(resampled_signals)), resampled_signals)
-plt.xlabel("Time(s)")
-plt.ylabel("Amp (v)")
-plt.subplot(122)
-plt.plot(np.arange(0, len(resampled_signals)), resampled_signals)
-plt.xlabel("Time(s)")
-plt.ylabel("Amp (v)")
-plt.show()
-
-# Signals DC Removal
-RemovedDC_signals = []
-DC_signal = [(resampled_signals[i] + 10) for i in range(len(resampled_signals))]
-for i in range(len(DC_signal)):
-    Mean = statistics.mean(DC_signal[i])
-    RemovedDC_signals.append([(DC_signal[i] - Mean) for i in range(len(DC_signal))])
-
-plt.plot(np.arange(0, len(RemovedDC_signals)), RemovedDC_signals[0])
-plt.xlabel("Time (s)")
-plt.ylabel("Amp (v)")
-plt.show()
+    for file in files:
+        temp_sig = []
+        for class_name in list_of_classes:
+            if file.startswith(class_name):
+                name = file.split('.')
+                channel_f.append(name[0][-1])  # list ['h' , 'v' , 'h' ,'v' , .....]
+                with open(path + "\\" + file, 'r') as f:
+                    for line in f:
+                        s = line.strip()  # Remove the newline character
+                        temp_sig.append(int(s))
+                signals_f.append(temp_sig)  # list of signals N x 251 where N is number of signals
+                signals_name_f.append(class_name)  # signal class ("asagi", "kirp", "sag", "sol", "yukari")
+                break
+    return signals_f, signals_name_f, channel_f
 
 
+signals, signals_name, channel = read_sig(path)
 
--- preprocessing.py
+++ preprocessing.py
@@ -5,23 +5,6 @@ import pandas as pd
 from scipy import signal
 from scipy.signal import butter, filtfilt
 
-EOG_signal = open("C:\\Users\\hooda\\Desktop\\EOG Vertical Signal.txt", "r")
-EOG_signal2 = open("C:\\Users\\hooda\\Desktop\\asagi1v.txt", "r")
-
-lines = EOG_signal.readlines()
-print(lines)
-
-Amp = []
-for i in range(len(lines) - 1):
-    L = lines[i + 1]
-    Amp.append(int(L))
-
-plt.figure(figsize=(12, 6))
-plt.plot(np.arange(0, len(Amp)), Amp)
-plt.xlabel('Time(s)')
-plt.xlabel('Amp (V)')
-plt.show()
-
 
 # 1) Filtering
 def butter_bandpass_filter(Input_Signal, Low_Cutoff, High_Cutoff, Sampling_Rate, order):
@@ -34,47 +17,28 @@ def butter_bandpass_filter(Input_Signal, Low_Cutoff, High_Cutoff, Sampling_Rate,
     return filtered
 
 
-# Invoking butter_bandpass filter and retrieving the EOG Filtered Signal
-filtered_Signal = butter_bandpass_filter(Amp, Low_Cutoff=0.5, High_Cutoff=20.0, Sampling_Rate=176, order=2)
-plt.figure(figsize=(12, 6))
-plt.plot(np.arange(0, len(filtered_Signal)), filtered_Signal)
-plt.xlabel("Time(s)")
-plt.ylabel("Amp (v)")
-plt.show()
-
 
 # 2) Resampling
-resampled_Signal = signal.resample(filtered_Signal, 50)
-plt.figure(figsize=(12, 6))
-plt.subplot(121)
-plt.plot(np.arange(0, len(filtered_Signal)), filtered_Signal)
-plt.xlabel("Time(s)")
-plt.ylabel("Amp (v)")
-plt.subplot(122)
-plt.plot(np.arange(0, len(resampled_Signal)), resampled_Signal)
-plt.xlabel("Time(s)")
-plt.ylabel("Amp (v)")
-plt.show()
+def Resampling(filtered_Signal):
+    resampled_Signal = signal.resample(filtered_Signal, 50)
+    plt.figure(figsize=(12, 6))
+    plt.subplot(121)
+    plt.plot(np.arange(0, len(filtered_Signal)), filtered_Signal)
+    plt.xlabel("Time(s)")
+    plt.ylabel("Amp (v)")
+    plt.subplot(122)
+    plt.plot(np.arange(0, len(resampled_Signal)), resampled_Signal)
+    plt.xlabel("Time(s)")
+    plt.ylabel("Amp (v)")
+    plt.show()
 
 
-# 3) DC component
-# Draw two signals
-plt.figure(figsize=(12, 6))
-plt.subplot(121)
-plt.plot(np.arange(0, len(filtered_Signal)), filtered_Signal)
-plt.xlabel("Time(s)")
-plt.ylabel("Amp (v)")
-
-plt.subplot(121)
-plt.plot(np.arange(0, len(filtered_Signal)), filtered_Signal)
-plt.xlabel("Time(s)")
-plt.ylabel("Amp (v)")
-
-# DC Remover
-DC_signal = [(filtered_Signal[i] + 10) for i in range(len(filtered_Signal))]
-Mean = statistics.mean(DC_signal)
-RemovedDC_signal = [(DC_signal[i] - Mean) for i in range(len(DC_signal))]
-plt.plot(np.arange(0, len(RemovedDC_signal)), RemovedDC_signal)
-plt.xlabel("Time (s)")
-plt.ylabel("Amp (v)")
-plt.show()
+# 3) DC Remover
+def DC_removal(filtered_Signal):
+    DC_signal = [(filtered_Signal[i] + 10) for i in range(len(filtered_Signal))]
+    Mean = statistics.mean(DC_signal)
+    RemovedDC_signal = [(DC_signal[i] - Mean) for i in range(len(DC_signal))]
+    plt.plot(np.arange(0, len(RemovedDC_signal)), RemovedDC_signal)
+    plt.xlabel("Time (s)")
+    plt.ylabel("Amp (v)")
+    plt.show()
